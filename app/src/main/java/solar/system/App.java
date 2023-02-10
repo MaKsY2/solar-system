b7160499bd623e682527f01d83e7dc858539a1a8
Michael <michael2ga3@gmail.com>
2023-02-10 19:58:51 +0300

    Add template window graphics

--- app/src/main/java/solar/system/App.java
+++ app/src/main/java/solar/system/App.java
@@ -2,5 +2,7 @@ package solar.system;
 
 public class App {
     public static void main(String[] args) {
+        var gr = new Graphics();
+        gr.run();
     }
 }
--- app/src/main/java/solar/system/Graphics.java
+++ app/src/main/java/solar/system/Graphics.java
@@ -1,4 +1,15 @@
 package solar.system;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 class Graphics {
+    public void run() {
+        JFrame frame = new JFrame();
+        frame.setBounds(10, 10, 500, 500);
+        frame.setDefaultCloseOperation(3);
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        frame.setVisible(true);
+    }
 }
